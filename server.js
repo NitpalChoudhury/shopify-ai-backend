8e19f005aa21a9791eec02baeba526ea6c7685c2
NitpalChoudhury <140379094+NitpalChoudhury@users.noreply.github.com>
2025-12-23 14:05:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const recommend = require("./recommend");
+const offer = require("./offer");
+const pool = require("./db");
+
+const app = express();
+app.use(cors());
+
+app.get("/recommend", async (req,res)=>{
+  const data = await recommend(req.query.pid);
+  res.json(data);
+});
+
+app.get("/offer", async (req,res)=>{
+  const data = await offer(req.query.pid, req.query.user);
+  res.json(data);
+});
+
+app.get("/track", async (req,res)=>{
+  await pool.query(
+    "INSERT INTO views(user_id,product_id) VALUES($1,$2)",
+    [req.query.user, req.query.pid]
+  );
+  res.json({success:true});
+});
+
+app.listen(process.env.PORT, ()=>{
+  console.log("server running");
+});
